f825fa8da9f4e332e961082c7331acd4247e675d
Paola Alapizco <pahoc.alapizco@gmail.com>
2020-05-28 01:05:20 -0600

    Modelo del registro de glucosa

---
+++ models/glurecord.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const {
+  DS_AYUNAS,
+  DS_ANTES_COMER,
+  DS_ANTER_COMER,
+  DS_DESPUES_COMER,
+  DS_DESPUES_CENAR
+} = require('../utils/globals');
+
+const Schema = mongoose.Schema;
+
+const dinnerStatusEnum = {
+  values: [DS_AYUNAS, DS_ANTES_COMER, DS_ANTER_COMER, DS_DESPUES_COMER, DS_DESPUES_CENAR],
+  message: '{VALUE} no es un role válido.'
+};
+
+const glurecord = new Schema({
+  day: {
+    type: Date,
+    required: [true, 'Falta registrar la fecha en que se realizó el test.']
+  },
+  hour: {
+    type: String,
+    required: [true, 'Falta registrar la hora en que se realizó el test.']
+  },
+  dinnerStatus: {
+    type: String,
+    enum: dinnerStatusEnum
+  },
+  level: {    
+    type: Number
+  }
+}, { timestamps: true });
+
+
+module.exports = mongoose.model('glucoserecord', glurecord);
---
+++ models/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+  GlucoseRecord: require('./glurecord'),
+};
